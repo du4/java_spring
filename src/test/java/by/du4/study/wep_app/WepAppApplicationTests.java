41fcec0053f37607ec1ac5e2d037ce70ed2cd92f
du4 <shkantau@gmail.com>
2021-12-01 22:33:21 +0300

    Init commit

---
+++ src/test/java/by/du4/study/wep_app/WepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.du4.study.wep_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
